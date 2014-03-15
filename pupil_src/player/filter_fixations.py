07ec32c58ceffc4d61748c5982b3317f6b70e851
Will Patera <willpatera@gmail.com>
2014-03-15 18:06:23 +0700

    incremental progress - fixations filtered based on dispersion & duration more to be done for filtering min_duration and display

--- pupil_src/player/filter_fixations.py
+++ pupil_src/player/filter_fixations.py
@@ -14,6 +14,7 @@ import numpy as np
 import atb
 from ctypes import c_float
 from methods import denormalize,normalize
+from player_methods import transparent_circle
 import logging
 from scan_path import Scan_Path
 
@@ -39,12 +40,22 @@ class Filter_Fixations(Plugin):
         + dispersion = how much movement is allowed within one fixation (e.g. > 8 pixels movement is no longer fixation)
         + duration = how long must recent_pupil_positions remain within dispersion threshold before classified as fixation (e.g. at least 100ms)
 
-    Smooth Pursuit (additional plugin to be run prior to fixations): 
-        + VOR - when moving the head while fixating on an object we need to compensate for scene/world movement (this as separate plugin?)
+    Smooth Pursuit/Ego-motion (additional plugin to be run prior to fixations): 
+        + VOR - when moving the head while fixating on an object we need to compensate for scene/world movement
+        + We compensate by using optical flow - sticking gaze points onto pixels in the scene in the Scan Path 
         Reference literature (Kinsman et al. "Ego-motion compensation improves fixation detection in wearable eye tracking," ACM 2011)
 
+    Overview:
+        + Get recent_pupil_positions from Scan Path plugin
+            + recent_pupil_positions gaze points "stuck" to the pixel to compensate for Ego-motion
+            + timeframe of Scan Path is the temporal window within which we calculate/determine fixations
+        + Use Sliding Filter to classify gaze point as fixation or saccade
+            + get gaze point at the middle of recent_pupil_positions
+            + create sub time slice of 400ms (200ms past, 200ms future)
+            + check distance between gaze point vs past & future
+            + check that time >= 0.1 second (approx. 3 frames)
     """
-    def __init__(self, g_pool=None,distance=25.0,gui_settings={'pos':(10,470),'size':(300,100),'iconified':False}):
+    def __init__(self, g_pool=None,distance=8.0,max_duration=400.0,gui_settings={'pos':(10,470),'size':(300,100),'iconified':False}):
         super(Filter_Fixations, self).__init__()
 
         self.g_pool = g_pool
@@ -53,6 +64,8 @@ class Filter_Fixations(Plugin):
 
         # user settings
         self.distance = c_float(float(distance))
+        self.max_duration = c_float(float(max_duration))
+        self.min_duration = 0.10
         self.gui_settings = gui_settings
 
         self.sp_active = True
@@ -77,23 +90,39 @@ class Filter_Fixations(Plugin):
         img = frame.img
         img_shape = img.shape[:-1][::-1] # width,height
 
-        filtered_gaze = []
+        if recent_pupil_positions:
+            past_gp = recent_pupil_positions[:len(recent_pupil_positions)/2]
+            curr_gp = recent_pupil_positions[len(recent_pupil_positions)/2]
+            future_gp = recent_pupil_positions[(len(recent_pupil_positions)/2)+1:]
 
-        for gp1, gp2 in zip(recent_pupil_positions[:-1], recent_pupil_positions[1:]):
-            gp1_norm = denormalize(gp1['norm_gaze'], img_shape,flip_y=True)
-            gp2_norm = denormalize(gp2['norm_gaze'], img_shape,flip_y=True)
-            x_dist =  abs(gp1_norm[0] - gp2_norm[0])
-            y_dist = abs(gp1_norm[1] - gp2_norm[1])
-            man = x_dist + y_dist
-            # print "man: %s\tdist: %s" %(man,self.distance.value)
-            if man < self.distance.value:
-                filtered_gaze.append(gp1)
-            else:
-                # print "filtered"
-                pass
+            now = curr_gp['timestamp']
+            past_cutoff = now-0.2 # 200 ms window/2 = max saccade duration
+            future_cutoff = now+0.2 # 
 
-        recent_pupil_positions[:] = filtered_gaze[:]
-        recent_pupil_positions.sort(key=lambda x: x['timestamp']) #this may be redundant...
+            past_gp = [g for g in past_gp if g['timestamp']>=past_cutoff and self.manhattan_dist_denormalize(curr_gp, g, img_shape) < self.distance.value]
+            future_gp = [g for g in future_gp if g['timestamp']<=future_cutoff and self.manhattan_dist_denormalize(curr_gp, g, img_shape) < self.distance.value] 
+            fixation_candidates = past_gp + future_gp
+
+            # if we detected a potential fixation, was it longer than 100 milliseconds?
+            future_t, past_t = False, False
+            if past_gp:
+                past_gp.sort(key=lambda x: x['timestamp'])
+                past_t = now-past_gp[0]['timestamp']
+                print "past_t: ", past_t
+
+            if future_gp:
+                future_gp.sort(key=lambda x: x['timestamp'])
+                future_t = future_gp[-1]['timestamp']-now
+
+            if future_t and past_t:
+                if future_t+past_t < 0.1:
+                    print "not a probable fixation..."
+                    print "fixation_time: ", future_t+past_t
+
+            # print "future_gp: ", future_gp
+
+            recent_pupil_positions[:] = fixation_candidates[:]
+            recent_pupil_positions.sort(key=lambda x: x['timestamp']) #this may be redundant...
 
 
 
@@ -124,7 +153,7 @@ class Filter_Fixations(Plugin):
 
 
     def get_init_dict(self):
-        d = {'distance':self.distance.value}
+        d = {'distance':self.distance.value, 'max_duration':self.max_duration.value}
         if hasattr(self,'_bar'):
             gui_settings = {'pos':self._bar.position,'size':self._bar.size,'iconified':self._bar.iconified}
             d['gui_settings'] = gui_settings
@@ -142,5 +171,11 @@ class Filter_Fixations(Plugin):
         """
         self._bar.destroy()
 
-
+    def manhattan_dist_denormalize(self, gp1, gp2, img_shape):
+        gp1_norm = denormalize(gp1['norm_gaze'], img_shape,flip_y=True)
+        gp2_norm = denormalize(gp2['norm_gaze'], img_shape,flip_y=True)
+        x_dist =  abs(gp1_norm[0] - gp2_norm[0])
+        y_dist = abs(gp1_norm[1] - gp2_norm[1])
+        man = x_dist + y_dist
+        return man
 
