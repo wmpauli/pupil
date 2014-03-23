b00749c9193a404e706a481049d4251a03524d78
Will Patera <willpatera@gmail.com>
2014-03-23 21:08:34 +0700

    incremental work - need to rewrite classifier

--- pupil_src/player/classify_fixations.py
+++ pupil_src/player/classify_fixations.py
@@ -43,7 +43,7 @@ class Classify_Fixations(Plugin):
     Terms
         + dispersion (spatial) = how much spatial movement is allowed within one fixation (in visual angular degrees or pixels)
         + duration (temporal) = what is the minimum time required for gaze data to be within dispersion threshold?
-        + cohesion (spatial+temporal) = is the cluster of 
+        + cohesion (spatial+temporal) = is the cluster of fixations close together
 
     Overview Diagram
         + Scan path supplies a window into the past set by user and supplies 'recent_pupil_positions' list (variable duration set by user)
@@ -52,9 +52,14 @@ class Classify_Fixations(Plugin):
         past[       recent_pupil_positions      ]current
             [-----------------p-----------------] 
 
+        + Starting from sample point 'p' walk forward and backward in time to check if fixation criteria (dispersion, duration) are satisfied
+
+        past[       recent_pupil_positions      ]current
+            [-----------------p-----------------] 
+
 
     """
-    def __init__(self, g_pool=None,distance=8.0,samp_width=300,show_saccades=False,gui_settings={'pos':(10,470),'size':(300,100),'iconified':False}):
+    def __init__(self, g_pool=None,dispersion_angle=1.0,img_width=1280,img_height=720,fov_angle=90.0,vis_sample_width=300,min_duration=0.0,max_duration=1.0,show_saccades=False,gui_settings={'pos':(10,470),'size':(300,200),'iconified':False}):
         super(Classify_Fixations, self).__init__()
 
         self.g_pool = g_pool
@@ -62,19 +67,26 @@ class Classify_Fixations(Plugin):
         self.order = .7
 
         # user settings
-        self.distance = c_float(float(distance))
-        self.samp_width = c_int(int(samp_width))
+        self.dispersion_angle = c_float(float(dispersion_angle)) # visual angle degrees
+        self.img_width = c_int(int(img_width))
+        self.img_height = c_int(int(img_height))
+        self.fov_angle = c_float((float(fov_angle)))
+
+        self.vis_sample_width = c_int(int(vis_sample_width))
+        self.min_duration = c_float(float(min_duration))
+        self.max_duration = c_float(float(max_duration))
         self.show_saccades = c_bool(bool(show_saccades))
-        self.min_duration = 0.10
-        self.max_duration = 0.40
         self.gui_settings = gui_settings
 
         self.sp_active = True
 
         # algorithm working data
-        self.raw_dispersion_history = {}
-        self.vis_dispersion_history = {}
-        self.timestamp_history = []
+        self.pix_per_degree = np.sqrt(self.img_width.value**2+self.img_height.value**2)/self.fov_angle.value
+        self.degree_per_pix = self.fov_angle.value/np.sqrt(self.img_width.value**2+self.img_height.value**2)
+        self.raw_dispersion_history = {} # k,v : timestamp as float, dispersion
+        self.vis_dispersion_history = {} # clone of raw_dispersion_history for visualization 
+        self.fixation_classification = {} # k,v : timestamp as float, 'fixation' or 'saccade' or 'unclassified'
+        self.timestamp_history = [] # list to keep track of timestamps already visited for quick dictionary access
         # degrees per pixel with c930e = 0.061282654954069871
         # 16.3 pixels = 1 degree 
         # 8 pixels = approx 0.5 degrees vis angle
@@ -83,7 +95,6 @@ class Classify_Fixations(Plugin):
         self.past_pt = None
         self.present_pt = None
         self.candidates = []
-
 
         #debug window
         self.suggested_size = 640,480
@@ -94,7 +105,7 @@ class Classify_Fixations(Plugin):
     def update(self,frame,recent_pupil_positions,events):
         img = frame.img
         img_shape = img.shape[:-1][::-1] # width,height
-        self.norm_distance = self.distance.value/img_shape[1]
+        self.dispersion_norm = normalize((0,self.dispersion_angle.value*self.pix_per_degree), img_shape) 
 
         # init debug window
         if self.window_should_open:
@@ -122,25 +133,49 @@ class Classify_Fixations(Plugin):
         # find distance between each gaze point pairwise
         for gp1, gp2 in zip(recent_pupil_positions[:-1], recent_pupil_positions[1:]):
             dispersion = self.manhattan_dist(gp1['norm_gaze'] ,gp2['norm_gaze'])
-            
+
+            # update index and dictionaries
             if gp1['timestamp'] not in self.timestamp_history:
                 self.raw_dispersion_history[gp1['timestamp']] = dispersion
                 self.vis_dispersion_history[gp1['timestamp']] = dispersion + 0.5
                 self.timestamp_history.append(gp1['timestamp']) 
 
-        # visualizations in debug window
+        # temporal window into the past is set by Scan Path
+        # get sample point closest to ideal midpoint of recent_pupil_positions
+        try:
+            recent_pupil_positions_duration = recent_pupil_positions[-1]['timestamp'] - recent_pupil_positions[0]['timestamp'] 
+            ideal_sample_time = recent_pupil_positions_duration*0.5
+            self.sample_pt = min(recent_pupil_positions, key=lambda k: abs(k['timestamp']-ideal_sample_time))                
+        except IndexError,e:
+            # index error will fire when recent_pupil_positions is None or < 2 items in list
+            pass
+
+        # print "normalized dispersion: ", self.dispersion_norm
+
+        for p in recent_pupil_positions:
+            # check dispersion
+            distance = self.manhattan_dist_denormalize(self.sample_pt['norm_gaze'], p['norm_gaze'], img_shape)
+            if distance <= self.dispersion_angle.value*self.pix_per_degree:
+                self.fixation_classification[p['timestamp']] = 'fixation'
+            else:
+                self.fixation_classification[p['timestamp']] = 'saccade'
+
+        # visualize dispersion and fixation classification in the debug window
         if self._window:
-            pts = zip( list(np.arange(0.,1.,1.0/self.samp_width.value)), map(self.vis_dispersion_history.get, self.timestamp_history[-self.samp_width.value:-1]) )
-            self.gl_display_in_window(pts)
+            # change sample width to time scale (then samples would not be evenly spaced)
+            raw_dispersion_pts = zip( list(np.arange(0.,1.,1.0/self.vis_sample_width.value)), map(self.vis_dispersion_history.get, self.timestamp_history[-self.vis_sample_width.value:]) )
+            fixation_pts = map(self.fixation_classification.get, self.timestamp_history[-self.vis_sample_width.value:])
+            fixation_pts =  zip( list(np.arange(0.,1.,1.0/self.vis_sample_width.value)), [0.6 if y is 'fixation' else 0.5 for y in fixation_pts])
+            # fixation_vis = zip( list(np.arange(0.,1.,1.0/self.vis_sample_width.value)), map(self.fixation_classification.get, self.timestamp_history[-self.vis_sample.width.value:]) )
+            self.gl_display_in_window(raw_dispersion_pts, fixation_pts)
 
         
 
         # for p in recent_pupil_positions:
-        #     p['dispersion'] = self.plugin_history.get(p['timestamp'],)
+        #     p['dispersion'] = self.plugin_history.get(p['timestamp'],'unclassified')
 
         recent_pupil_positions.sort(key=lambda k: k['timestamp']) #this may be redundant...
         # logger.debug("dict: %s" %(self.d))
-
 
         # # current hack for drawing fixations and saccades without vis_circle
         # pts = [p for p in recent_pupil_positions if p.has_key('type') and p['type'] is 'fixation']
@@ -164,10 +199,18 @@ class Classify_Fixations(Plugin):
             text='light', position=pos,refresh=.1, size=self.gui_settings['size'])
 
         self._bar.iconified = self.gui_settings['iconified']
-        self._bar.add_var('dispersion in pixels',self.distance,min=0,step=0.1)
-        self._bar.add_var('debug sample width',self.samp_width,min=10,step=1)
+        self._bar.add_var('dispersion visual angle degree',self.dispersion_angle,min=0,step=0.1)
+        self._bar.add_var('image width',self.img_width,min=0,step=1)
+        self._bar.add_var('image height',self.img_height,min=0,step=1)
+        self._bar.add_var('field of view angle',self.img_height,min=0,step=0.5)        
+
+        self._bar.add_var('visualization sample width',self.vis_sample_width,min=10,step=1)
+        self._bar.add_button("open debug window", self.toggle_window,help="Visualization of fixation classification data.")
+        self._bar.add_var("min duration", self.min_duration,min=0.0,max=self.max_duration.value,step=0.1)
+        self._bar.add_var("max duration", self.max_duration,min=self.min_duration.value,max=2.0,step=0.1)
+
+
         self._bar.add_var('show saccades',self.show_saccades)
-        self._bar.add_button("open debug window", self.toggle_window,help="Visualization of gaze velocity/time.")
         self._bar.add_button('remove',self.unset_alive)
    
     def toggle_window(self):
@@ -226,7 +269,7 @@ class Classify_Fixations(Plugin):
             self._window = None
             self.window_should_close = False
 
-    def gl_display_in_window(self,pts):
+    def gl_display_in_window(self,pts, fixation_pts):
         active_window = glfwGetCurrentContext()
         glfwMakeContextCurrent(self._window)
         clear_gl_screen()
@@ -234,7 +277,8 @@ class Classify_Fixations(Plugin):
         # draw_gl_texture(img,interpolation=False)
         offset = 0.5
         draw_gl_polyline_norm(([0.0,offset],[1.0,offset]),(.0,.0,.0,0.5),type="Strip")
-        draw_gl_polyline_norm(([0.0,self.norm_distance+offset],[1.0,self.norm_distance+offset]),(1.0,0.4,.0,0.5),type="Strip")
+        draw_gl_polyline_norm(([0.0,self.dispersion_norm[1]+offset],[1.0,self.dispersion_norm[1]+offset]),(1.0,0.4,.0,0.5),type="Strip")
+        draw_gl_polyline_norm(fixation_pts, (0.,0.,1.,1.))
 
         draw_gl_polyline_norm(pts,(0.,1.,0,1.),type="Strip")
         glfwSwapBuffers(self._window)
@@ -253,7 +297,7 @@ class Classify_Fixations(Plugin):
 
 
     def get_init_dict(self):
-        d = {'distance':self.distance.value, 'show_saccades':self.show_saccades.value}
+        d = {'distance':self.dispersion_angle.value}
         if hasattr(self,'_bar'):
             gui_settings = {'pos':self._bar.position,'size':self._bar.size,'iconified':self._bar.iconified}
             d['gui_settings'] = gui_settings
@@ -274,8 +318,8 @@ class Classify_Fixations(Plugin):
         self._bar.destroy()
 
     def manhattan_dist_denormalize(self, gp1, gp2, img_shape):
-        gp1_norm = denormalize(gp1['norm_gaze'], img_shape,flip_y=True)
-        gp2_norm = denormalize(gp2['norm_gaze'], img_shape,flip_y=True)
+        gp1_norm = denormalize(gp1, img_shape, flip_y=True)
+        gp2_norm = denormalize(gp2, img_shape, flip_y=True)
         x_dist =  abs(gp1_norm[0] - gp2_norm[0])
         y_dist = abs(gp1_norm[1] - gp2_norm[1])
         man = x_dist + y_dist
